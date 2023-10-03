1d9888118a60abf532a150949c46364077ee4d01
Wilkys Leal <wilkysdasilvaleal@gmail.com>
2023-10-03 16:15:22 -0300

    Peimeiro Projeto

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+main(){
+  runApp(Container(
+    child: Center(
+      child: Text(
+      'Wilkys Leal',
+      textDirection: TextDirection.ltr,
+      style: TextStyle(color: Colors.white, fontSize: 50.0)
+      ),
+    ),
+  ));
+}
